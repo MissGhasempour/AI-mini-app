8245f2e8b5d49db8bf1360f184720516562cac66
MissGhasempour <romi.ghasem@gmail.com>
2025-06-09 16:25:10 +0330

    login component created + react-router-dom added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import "./App.css";
 import ManageMessage from "./components/messages/ManageMessage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/register/Login";
 
 function App() {
-  return <>
-    <ManageMessage />
-  </>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<ManageMessage />} path="/" />
+        <Route element={<Login />} path="/login" />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
